5b6bede4532fc523596384cb0a94e45ebcdb15b5
Maureen Moore <makamo66@hotmail.com>
2016-08-01 15:56:38 -0400

    Created admin-teacher.html

--- quaza/js/hr.js
+++ quaza/js/hr.js
@@ -1,10 +1,15 @@
 function hrFunction1() {
 	if( $("#hr-1").hasClass("black-hr-1") ) {
 		$("#hr-1").removeClass("black-hr-1").addClass("green-hr-1");
+		$("#title-1").removeClass("black").addClass("green");
 		$("#hr-2").removeClass("green-hr-1").addClass("black-hr-1");
+		$("#title-2").removeClass("green").addClass("black");
 		$("#hr-3").removeClass("green-hr-1").addClass("black-hr-1");
+		$("#title-3").removeClass("green").addClass("black");
 } else {
+		$("#title-1").removeClass("black").addClass("green");
 		$("#hr-1").removeClass("black-hr-1").addClass("green-hr-1");
+
 }
 }
 
@@ -12,6 +17,10 @@ function hrFunction2() {
 		$("#hr-1").removeClass("green-hr-1").addClass("black-hr-1");
 		$("#hr-2").removeClass("black-hr-1").addClass("green-hr-1");
 		$("#hr-3").removeClass("green-hr-1").addClass("black-hr-1");
+		$("#title-1").removeClass("green").addClass("black");
+		$("#title-2").removeClass("black").addClass("green");
+		$("#title-3").removeClass("green").addClass("black");
+
 }
 
 function hrFunction3() {
@@ -19,8 +28,52 @@ function hrFunction3() {
 		$("#hr-3").removeClass("black-hr-1").addClass("green-hr-1");
 		$("#hr-2").removeClass("green-hr-1").addClass("black-hr-1");
 		$("#hr-1").removeClass("green-hr-1").addClass("black-hr-1");
+		$("#title-3").removeClass("black").addClass("green");
+		$("#title-2").removeClass("green").addClass("black");
+		$("#title-1").removeClass("green").addClass("black");
 } else {
 		$("#hr-3").removeClass("black-hr-1").addClass("green-hr-1");
+		$("#title-3").removeClass("black").addClass("green");
+}
+}
+
+
+function hrFunction1a() {
+	if( $("#hr-1").hasClass("black-hr-1a") ) {
+		$("#hr-1").removeClass("black-hr-1a").addClass("green-hr-1a");
+		$("#title-1").removeClass("black").addClass("green");
+		$("#hr-2").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#title-2").removeClass("green").addClass("black");
+		$("#hr-3").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#title-3").removeClass("green").addClass("black");
+} else {
+		$("#title-1").removeClass("black").addClass("green");
+		$("#hr-1").removeClass("black-hr-1a").addClass("green-hr-1a");
+
+}
+}
+
+function hrFunction2a() {
+		$("#hr-1").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#hr-2").removeClass("black-hr-1a").addClass("green-hr-1a");
+		$("#hr-3").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#title-1").removeClass("green").addClass("black");
+		$("#title-2").removeClass("black").addClass("green");
+		$("#title-3").removeClass("green").addClass("black");
+
+}
+
+function hrFunction3a() {
+	if( $("#hr-3").hasClass("black-hr-1a") ) {
+		$("#hr-3").removeClass("black-hr-1a").addClass("green-hr-1a");
+		$("#hr-2").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#hr-1").removeClass("green-hr-1a").addClass("black-hr-1a");
+		$("#title-3").removeClass("black").addClass("green");
+		$("#title-2").removeClass("green").addClass("black");
+		$("#title-1").removeClass("green").addClass("black");
+} else {
+		$("#hr-3").removeClass("black-hr-1a").addClass("green-hr-1a");
+		$("#title-3").removeClass("black").addClass("green");
 }
 }
 function hrFunctionA() {
