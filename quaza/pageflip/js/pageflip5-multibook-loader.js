ad1dd8f6565b8f915382ecdde36ce22108c199b5
Maureen Moore <makamo66@hotmail.com>
2016-09-07 16:10:23 -0400

    Dis-abled right click for both flip books

--- quaza/pageflip/js/pageflip5-multibook-loader.js
+++ quaza/pageflip/js/pageflip5-multibook-loader.js
@@ -132,3 +132,14 @@ $( function() {
 	startPageflip( startID );
 
 });
+
+document.onmousedown=disableclick;
+status="Right Click Disabled";
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert(status);
+     return false;    
+   }
+}
--- quaza/turn/js/basic.js
+++ quaza/turn/js/basic.js
@@ -92,3 +92,14 @@ function isChrome() {
 	return navigator.userAgent.indexOf('Chrome')!=-1;
 
 }
+
+document.onmousedown=disableclick;
+status="Right Click Disabled";
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert(status);
+     return false;    
+   }
+}
