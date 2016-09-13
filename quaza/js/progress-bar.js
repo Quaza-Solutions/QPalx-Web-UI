372e6d859875adc88918877748cf3e4ad2d610ac
Maureen Moore <makamo66@hotmail.com>
2016-09-13 08:37:07 -0400

    Sped up progress bars at home-add-courses.html

--- quaza/js/progress-bar.js
+++ quaza/js/progress-bar.js
@@ -5,7 +5,8 @@
           progressbarValue = progressbar.next(),
           value = progressbar.data('min') || 0,
           max = progressbar.data('max'),
-          time = (1000 / max) * 5,
+         // time = (1000 / max) * 5,
+		 time = (400 / max) * 5,
           value = progressbar.val(),
           animate = setInterval(loading, time);
 
