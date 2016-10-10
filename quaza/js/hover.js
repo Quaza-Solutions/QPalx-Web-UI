e000e19244883fff5d8eaadd8ff880019fbb55cf
Maureen Moore <makamo66@hotmail.com>
2016-10-10 15:22:28 -0400

    Added tool tips to micro-lesson-display.html

--- quaza/js/hover.js
+++ quaza/js/hover.js
@@ -1,4 +1,40 @@
 
+		 $(".anchor-1").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-1').append(newDiv3);
+});
+		 $(".anchor-2").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-2').append(newDiv3);
+});	
+		 $(".anchor-3").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-3').append(newDiv3);
+});
+		 $(".anchor-4").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-4').append(newDiv3);
+});
+		 $(".anchor-5").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-5').append(newDiv3);
+});
+		 $(".anchor-6").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-6').append(newDiv3);
+});
+		 $(".anchor-7").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson. ";
+		var newDiv3 = $('<div style=\"position: absolute; top: 0px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		$('.anchor-default-7').append(newDiv3);
+});
+		 
 	 $("button").hover(function() {
   	$(this).removeClass("btn-default").addClass("btn-danger");
 
@@ -53,4 +89,14 @@
 		$(".default-btn-6").empty();
 		$(".default-btn-7").empty();
 		$(this).removeClass("btn-danger").addClass("btn-default");
+		});
+		 
+	 	 $("a").mouseout(function() {
+		$(".anchor-default-1").empty();
+		$(".anchor-default-2").empty();
+		$(".anchor-default-3").empty();
+		$(".anchor-default-4").empty();
+		$(".anchor-default-5").empty();
+		$(".anchor-default-6").empty();
+		$(".anchor-default-7").empty();
 		});
