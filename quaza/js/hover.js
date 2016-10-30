0202e9a59a81ec1665df0fa2eee8a860bff6d1ba
Maureen Moore <makamo66@hotmail.com>
2016-10-30 13:31:34 -0400

    Added tooltips and API call to micro-lesson-display-2.html and quiz-display-page.html

--- quaza/js/hover.js
+++ quaza/js/hover.js
@@ -1,37 +1,125 @@
+		 $(".audio-1").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-1').append(newDiv3);
+});
+		 
+		 $(".audio-2").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-2').append(newDiv3);
+});
+		 
+		 $(".audio-3").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-3').append(newDiv3);
+});
+		 
+		 $(".audio-4").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-4').append(newDiv3);
+});
+		 
+		 $(".audio-5").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-5').append(newDiv3);
+});
+		 
+		 $(".audio-6").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-6').append(newDiv3);
+});
 
+		 $(".audio-7").hover(function() {	
+		var txt3 = "Click here to launch mini-lesson with narration. ";
+		var newDiv3 = $('<div class=\"audio-icon\"></div>').text(txt3);
+		$('.audio-default-7').append(newDiv3);
+});
+		 
+		$(".hand-1").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-1').append(newDiv3);
+});
+
+		$(".hand-2").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-2').append(newDiv3);
+});
+		
+		$(".hand-3").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-3').append(newDiv3);
+});
+		
+		$(".hand-4").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-4').append(newDiv3);
+});
+		
+		$(".hand-5").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-5').append(newDiv3);
+});
+		
+		$(".hand-6").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-6').append(newDiv3);
+});
+		
+		$(".hand-7").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-7').append(newDiv3);
+});
+		
+		$(".hand-1").hover(function() {	
+		var txt3 = "Click here to launch interactive mini-lesson. ";
+		var newDiv3 = $('<div class=\"hand-icon\"></div>').text(txt3);
+		$('.hand-default-1').append(newDiv3);
+});
 		 $(".anchor-1").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-1').append(newDiv3);
 });
 		 $(".anchor-2").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-2').append(newDiv3);
 });	
 		 $(".anchor-3").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-3').append(newDiv3);
 });
 		 $(".anchor-4").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-4').append(newDiv3);
 });
 		 $(".anchor-5").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-5').append(newDiv3);
 });
 		 $(".anchor-6").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-6').append(newDiv3);
 });
 		 $(".anchor-7").hover(function() {	
 		var txt3 = "Click here to launch mini-lesson. ";
-		var newDiv3 = $('<div style=\"position: absolute; top: -20px; left: 140px; background-color: #444; padding: 4px; width: 280px; height: 30px; color: #fff;\"></div>').text(txt3);
+		var newDiv3 = $('<div class=\"microlesson\"></div>').text(txt3);
 		$('.anchor-default-7').append(newDiv3);
 });
 		 
@@ -99,4 +187,26 @@
 		$(".anchor-default-5").empty();
 		$(".anchor-default-6").empty();
 		$(".anchor-default-7").empty();
+		});
+		 
+		 
+		 
+	 	 $("a").mouseout(function() {
+		$(".audio-default-1").empty();
+		$(".audio-default-2").empty();
+		$(".audio-default-3").empty();
+		$(".audio-default-4").empty();
+		$(".audio-default-5").empty();
+		$(".audio-default-6").empty();
+		$(".audio-default-7").empty();
+		});
+		 
+	 	 $("a").mouseout(function() {
+		$(".hand-default-1").empty();
+		$(".hand-default-2").empty();
+		$(".hand-default-3").empty();
+		$(".hand-default-4").empty();
+		$(".hand-default-5").empty();
+		$(".hand-default-6").empty();
+		$(".hand-default-7").empty();
 		});
