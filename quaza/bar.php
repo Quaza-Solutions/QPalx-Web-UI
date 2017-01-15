e765a6a3bf239a27a2adb5778b20e1d743e2a0b8
Maureen Moore <makamo66@hotmail.com>
2017-01-15 15:24:26 -0500

    Added two new graphs to bar.php

--- quaza/bar.php
+++ quaza/bar.php
@@ -1,11 +1,7 @@
-
-
 <?php
 
 session_start();
-/*
-if (isset($_POST["submit"]) && !empty($_POST["title"]) && !empty($_POST["selection1"])&& !empty($_POST["proportion1"])&& !empty($_POST["selection2"])&& !empty($_POST["proportion2"])&& !empty($_POST["selection3"])&& !empty($_POST["proportion3"])&& !empty($_POST["selection4"])&& !empty($_POST["proportion4"])&& !empty($_POST["selection5"])&& !empty($_POST["proportion5"])) {
-*/
+
 if (isset($_POST["submit"])) {
 	
 $title = $_POST['title'];
@@ -95,9 +91,116 @@ $proportion5 = $_SESSION['proportion5'];
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:th="http://www.thymeleaf.org">
  
   <head>
-    <!--Load the AJAX API-->
+	
+    
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+<style>
+
+nav ul ul {
+	display: none;
+}
+
+	nav ul li:hover > ul {
+		display: block;
+	}
+nav ul {
+	background: #efefef; 
+	background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+	background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+	background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+	box-shadow: 0px 0px 9px rgba(0,0,0,0.15);
+	padding: 0 20px;
+	border-radius: 10px;  
+	list-style: none;
+	position: relative;
+	display: inline-table;
+}
+	nav ul:after {
+		content: ""; clear: both; display: block;
+	}	
+nav ul li {
+	float: left;
+}
+	nav ul li:hover {
+		background: #4b545f;
+		background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+		background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+		background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+	}
+		nav ul li:hover a {
+			color: #fff;
+		}
+	
+	nav ul li a {
+		display: block; padding: 25px 40px;
+		color: #757575; text-decoration: none;
+	}
+</style>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
+
+ <script type="text/javascript">
+
+//vertical chart begin
+
+<?php if ( !empty($_REQUEST["vertical"]) ) { ?> 
+
+   google.charts.load('current', {packages:['corechart']});
+      google.charts.setOnLoadCallback(drawStuff);
+        function drawStuff() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', '<?php echo $selection; ?>');
+          data.addColumn('number', '<?php echo $name; ?>');
+          data.addRows([
+			<?php if(!empty($selection1)) { ?>
+            ['<?php echo $selection1; ?>', <?php echo $proportion1; ?>],
+			<?php } ?>
+			<?php if(!empty($selection2)) { ?>
+            ['<?php echo $selection2; ?>', <?php echo $proportion2; ?>],
+			<?php } ?>
+			<?php if(!empty($selection3)) { ?>
+            ['<?php echo $selection3; ?>', <?php echo $proportion3; ?>],
+			<?php } ?>
+			<?php if(!empty($selection4)) { ?>
+            ['<?php echo $selection4; ?>', <?php echo $proportion4; ?>],
+			<?php } ?>
+			<?php if(!empty($selection2)) { ?>
+            ['<?php echo $selection5; ?>', <?php echo $proportion5; ?>]
+			<?php } ?>
+          ]);
+
+         var options = {
+           title: '<?php echo $title; ?>',
+           width: 900,
+           height: 300,
+           legend: 'none',
+           bar: {groupWidth: '95%'},
+           vAxis: { gridlines: { count: 4 } }
+         };
+
+         var chart = new google.visualization.ColumnChart(document.getElementById('number_format_chart'));
+         chart.draw(data, options);
+
+         document.getElementById('format-select').onchange = function() {
+           options['vAxis']['format'] = this.value;
+           chart.draw(data, options);
+         };
+      };
+	  
+	  //vertical chart end
+
+
+       // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+
+<?php } 
+
+
+elseif ( !empty($_REQUEST["horizontal"]) && isset($_POST["submit"]) ) { ?> 	
+ 
+ //horizontal chart begin
 
       // Load the Visualization API and the corechart package.
       google.charts.load('current', {'packages':['corechart']});
@@ -145,10 +248,56 @@ $proportion5 = $_SESSION['proportion5'];
         var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
-    </script>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
+
+ //horizontal chart end
+
+<?php }  elseif ( !empty($_REQUEST["pie"])  ) { ?> 	
+       // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', '<?php echo $selection; ?>');
+        data.addColumn('number', '<?php echo $name; ?>');
+        data.addRows([
+		<?php if(!empty($selection1)) { ?>
+          ['<?php echo $selection1; ?>', <?php echo $proportion1; ?>],
+		<?php } ?>
+		<?php if(!empty($selection2)) { ?>
+          ['<?php echo $selection2; ?>', <?php echo $proportion2; ?>],
+		<?php } ?>
+		<?php if(!empty($selection3)) { ?>
+          ['<?php echo $selection3; ?>', <?php echo $proportion3; ?>],
+		<?php } ?>
+		<?php if(!empty($selection4)) { ?>
+          ['<?php echo $selection4; ?>', <?php echo $proportion4; ?>],
+		<?php } ?>
+		<?php if(!empty($selection5)) { ?>
+          ['<?php echo $selection5; ?>', <?php echo $proportion5; ?>]
+		<?php } ?>
+        ]);
+
+        // Set chart options
+        var options = {'title':'<?php echo $title ?>',
+                       'width':800,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+
+<?php }  ?>	 
+      </script>   
   </head>
   <body>
  <header>
@@ -163,26 +312,258 @@ $proportion5 = $_SESSION['proportion5'];
   </div>
   </form>
   </header>
-<div class="container" style="margin: 20px auto;">
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-   <p>Title of chart&nbsp;&nbsp;<input type="text" name="title"></p>
- <p>Components&nbsp;&nbsp;<input type="text" name="name"></p>
- <p>Name of Columns&nbsp;&nbsp;<input type="text" name="selection"></p>
-   <p>Selection 1&nbsp;&nbsp;<input type="text" name="selection1"></p>
-      <p>Proportion 1&nbsp;&nbsp;<input type="text" name="proportion1"></p>
-    <p>Selection 2&nbsp;&nbsp;<input type="text" name="selection2"></p>
-      <p>Proportion 2&nbsp;&nbsp;<input type="text" name="proportion2"></p>
-    <p>Selection 3&nbsp;&nbsp;<input type="text" name="selection3"></p>
-      <p>Proportion 3&nbsp;&nbsp;<input type="text" name="proportion3"></p>
-    <p>Selection 4&nbsp;&nbsp;<input type="text" name="selection4"></p>
-      <p>Proportion 4&nbsp;&nbsp;<input type="text" name="proportion4"></p>
-    <p>Selection 5&nbsp;&nbsp;<input type="text" name="selection5"></p>
-      <p>Proportion 5&nbsp;&nbsp;<input type="text" name="proportion5"></p>
-   <input type="submit" name="submit" value="Submit Form"><br>
-</form>
-</div>
+<div class="container" style="margin: 20px auto; text-align: center;">
+
+ <nav>
+	<ul>
+		<li><a href="#popup1">Horizontal Bar Chart</a></li>
+		<li><a href="#popup2">Vertical Bar Chart</a></li>
+		<li><a href="#popup3">Pie Chart</a></li>
+	</ul>
+</nav>
+ </div>
+ <div class="container" style="margin: 20px auto;">
     <!--Div that will hold the pie chart-->
     <div id="chart_div"></div>
+    <div id="number_format_chart"></div>
+    </div>
+    
+ <div id="popup1" class="modalBox">
+ <a href="#" class="modalCloseOuter"></a>
+ <div class="charts">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   <p>Title of chart (required)<br /><input type="text" name="title"></p>
+   </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Components (required)<br /><input type="text" name="name"></p>
+ </div>
+ </div>
+ <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Column names (required)<br /><input type="text" name="selection"></p>
+ </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <p>Chart Element Value 1&nbsp;&nbsp;<br /><input type="text" name="selection1"></p>
+      
+      </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 1&nbsp;&nbsp;<br /><input type="text" name="proportion1"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 2&nbsp;&nbsp;<br /><input type="text" name="selection2"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 2&nbsp;&nbsp;<br /><input type="text" name="proportion2"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 3&nbsp;&nbsp;<br /><input type="text" name="selection3"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 3&nbsp;&nbsp;<br /><input type="text" name="proportion3"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 4&nbsp;&nbsp;<br /><input type="text" name="selection4"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 4&nbsp;&nbsp;<br /><input type="text" name="proportion4"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 5&nbsp;&nbsp;<br /><input type="text" name="selection5"></p>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 5&nbsp;&nbsp;<br /><input type="text" name="proportion5"></p>
+<input type="hidden" name="horizontal" value="horizontal">
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">  
+   <input type="submit" name="submit" value="Submit Form"><br>
+   </div>
+   </div>
+</form>
+  </div>
+  </div>
+
+ <div id="popup3" class="modalBox">
+ <a href="#" class="modalCloseOuter"></a>
+ <div class="charts">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   <p>Title of chart (required)<br /><input type="text" name="title"></p>
+   </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Components (required)<br /><input type="text" name="name"></p>
+ </div>
+ </div>
+ <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Column names (required)<br /><input type="text" name="selection"></p>
+ </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <p>Chart Element Value 1&nbsp;&nbsp;<br /><input type="text" name="selection1"></p>
+      
+      </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 1&nbsp;&nbsp;<br /><input type="text" name="proportion1"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 2&nbsp;&nbsp;<br /><input type="text" name="selection2"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 2&nbsp;&nbsp;<br /><input type="text" name="proportion2"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 3&nbsp;&nbsp;<br /><input type="text" name="selection3"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 3&nbsp;&nbsp;<br /><input type="text" name="proportion3"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 4&nbsp;&nbsp;<br /><input type="text" name="selection4"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 4&nbsp;&nbsp;<br /><input type="text" name="proportion4"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 5&nbsp;&nbsp;<br /><input type="text" name="selection5"></p>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 5&nbsp;&nbsp;<br /><input type="text" name="proportion5"></p>
+<input type="hidden" name="pie" value="pie">
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">  
+   <input type="submit" name="submit" value="Submit Form"><br>
+   </div>
+   </div>
+</form>
+  </div>
+  </div>
+  
+ <div id="popup2" class="modalBox">
+ <a href="#" class="modalCloseOuter"></a>
+ <div class="charts">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   <p>Title of chart (required)<br /><input type="text" name="title"></p>
+   </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Components (required)<br /><input type="text" name="name"></p>
+ </div>
+ </div>
+ <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Column names (required)<br /><input type="text" name="selection"></p>
+ </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+   
+   </div>
+   </div>
+   <div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+      <p>Chart Element Value 1&nbsp;&nbsp;<br /><input type="text" name="selection1"></p>
+      
+      </div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 1&nbsp;&nbsp;<br /><input type="text" name="proportion1"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 2&nbsp;&nbsp;<br /><input type="text" name="selection2"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 2&nbsp;&nbsp;<br /><input type="text" name="proportion2"></p>
+   
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+ <p>Chart Element Value 3&nbsp;&nbsp;<br /><input type="text" name="selection3"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 3&nbsp;&nbsp;<br /><input type="text" name="proportion3"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 4&nbsp;&nbsp;<br /><input type="text" name="selection4"></p>
+      
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 4&nbsp;&nbsp;<br /><input type="text" name="proportion4"></p>
+    
+</div>
+</div>
+<div class="row">
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Chart Element Value 5&nbsp;&nbsp;<br /><input type="text" name="selection5"></p>
+</div>
+<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+<p>Proportion 5&nbsp;&nbsp;<br /><input type="text" name="proportion5"></p>
+<input type="hidden" name="vertical" value="vertical">
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">  
+   <input type="submit" name="submit" value="Submit Form"><br>
+   </div>
+   </div>
+</form>
+  </div>
+  </div>
+  
+
+
 <div class="row bottom-6">
 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 bottom-4">
 <img src="assets/Social-Icons-01.png" class="social-icons" />
@@ -209,6 +590,10 @@ latest news</p>
 </div>
 <footer>
 </footer>
+
+    <!--Load the AJAX API-->
+
+
  </body>
 </html>
 
